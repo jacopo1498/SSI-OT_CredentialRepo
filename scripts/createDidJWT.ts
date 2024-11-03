9e66fc1eb2c02f4447cfba44eb1fc72ebc912138
jacopo1498 <jacopo1498@gmail.com>
2024-11-03 19:35:12 +0100

    vc creation and verifing working correctly

--- scripts/createDidJWT.ts
+++ scripts/createDidJWT.ts
@@ -4,24 +4,21 @@ const didJWT = require('did-jwt');
 const { ethers } = require("hardhat");
 import { computePublicKey } from '@ethersproject/signing-key'
 import { EthrDID } from 'ethr-did'
-import { createJWT, verifyJWT } from "did-jwt";
 
 
 async function main() {
     const registryAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
     const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
     //const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; //Account 0
-    const privateKey = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"; //Account 0
-    const wallet = new ethers.Wallet(privateKey, provider);
+    const privateKey = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"; //Account 1
     const chainId = 31337;
 
     const publicKey = computePublicKey(privateKey, true);
-    const uncompressedPublicKey = computePublicKey(privateKey, false);
     console.log("Public Key: "+publicKey);
     console.log("Private Key: "+privateKey);
     const identifier = `did:ethr:${chainId}:${publicKey}`;
     //const signer = provider.getSigner(0); //for this experiment i am using account #0
-    const signer = provider.getSigner(1); //for this experiment i am using account #0
+    const signer = provider.getSigner(1); //for this experiment i am using account #1
     let signJ=didJWT.ES256KSigner(Buffer.from(privateKey.slice(2), 'hex'),false);
 
     // Set up DID resolver
--- scripts/createsignVC.ts
+++ scripts/createsignVC.ts
@@ -1,14 +1,28 @@
 import { EthrDID } from 'ethr-did'
 import { Issuer } from 'did-jwt-vc'
-import { JwtCredentialPayload, createVerifiableCredentialJwt } from 'did-jwt-vc'
+import { Resolver } from 'did-resolver'
+import getResolver from 'ethr-did-resolver'
+import { JwtCredentialPayload, createVerifiableCredentialJwt, verifyCredential } from 'did-jwt-vc'
 
 const issuer = new EthrDID({
   identifier: "did:ethr:0x7a69:0x038318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed75",
   privateKey: 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 }) as Issuer
 
-var x = Date.now() / 1000;
+const registryAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+const chainId = 31337;
 
+// Set up  resolver
+const vcResolver = getResolver.getResolver(
+    {
+        rpcUrl: "http://127.0.0.1:8545",
+        registry: registryAddress,
+        chainId: chainId,
+        provider
+    }
+);
+const Res = new Resolver(vcResolver)
 
 const vcPayload: JwtCredentialPayload = {
     sub: 'did:ethr:0x7a69:0x02ba5734d8f7091719471e7f7ed6b9df170dc70cc661ca05e688601ad984f068b0',
@@ -28,5 +42,7 @@ const vcPayload: JwtCredentialPayload = {
 (async () => {
     const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer);
     console.log(vcJwt);
+    const verifiedCredential= await verifyCredential(vcJwt, Res,{});
+    console.log(verifiedCredential)
 })();
 
